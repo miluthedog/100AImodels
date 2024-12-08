41580df2d2c59e8134571a4537c0a4c94190d74d
Pha <anhphamhoang04@gmail.com>
2024-12-08 21:53:23 +0700

    change path

--- facedetectYOLO/testYOLOv11.py
+++ facedetectYOLO/testYOLOv11.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("facedetectYOLO/runs/detect/train2/weights/best.pt")
+model = YOLO("facedetectYOLO/runs/detect/train/weights/best.pt")
 
 result = model(source=0,show=True,conf=0.7)
