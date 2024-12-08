dbf2aa12bf0d923226a351bf89aad975a185eff6
Pha <anhphamhoang04@gmail.com>
2024-12-08 21:56:06 +0700

    model 2

--- facedetectYOLO/testYOLOv11.py
+++ facedetectYOLO/testYOLOv11.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("facedetectYOLO/runs/detect/train/weights/best.pt")
+model = YOLO("facedetectYOLO/best.pt")
 
 result = model(source=0,show=True,conf=0.7)
